6eaa52599cdaa1ae455e840eddf0f17e80ac23c7
lvv5040 <66978068+lvv5040@users.noreply.github.com>
2020-08-31 18:40:49 -0600

    test

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name="", id=0, email="") {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return "Employee";
+    }
+
+}
+
+module.export=Employee
